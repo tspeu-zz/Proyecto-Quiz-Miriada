dc835d73900d05e3889e4f3f82641ef45da9c4b0
JM_B <tenshotseuropa@gmail.com>
2015-07-11 23:01:42 +0100

    Incluye Pagina autor, footer, Modulo 6 acabado

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,20 @@ var router = express.Router();
 var quizController = require('../controllers/quiz_controller');
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Quiz' });
+  res.render('index', { title: 'QUIZ' });
 });
+
+
 //hace el get de question
 router.get('/quizes/question', quizController.question);
 //hace el get que answer
 router.get('/quizes/answer', quizController.answer);
+
+/* GETp pagina creditos. */
+router.get('/autor', function(req, res) {
+  res.render('autor', { nombre : 'MIRIADA Módulo 6',
+                        autor : 'Juan M Banchero'
+                      });
+});
 
 module.exports = router;
