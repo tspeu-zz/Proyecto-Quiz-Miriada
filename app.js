7007ef2d1263ba68b154296b419445a9acce48a8
JM_B <tenshotseuropa@gmail.com>
2015-07-11 17:49:07 +0100

    Pasos 3-4 agregar express-partials

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var partials = require('express-partials');
 var routes = require('./routes/index');
 
 var app = express();
@@ -21,7 +21,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(partials());
 app.use('/', routes);
 
 
